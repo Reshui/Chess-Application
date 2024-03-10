a05e9da20b98e120bfcd11acfbf62705496fb6f9
Reshui <yliyahmiller@outlook.comm>
2024-03-10 06:39:38 -0600

    Adjusted code for an additional stalemate condition.

--- GameEnvironment.cs
+++ GameEnvironment.cs
@@ -27,7 +27,7 @@ public class GameEnvironment
     public Dictionary<Team, Player> AssociatedPlayers = new();
 
     /// <summary>Dictionary of <see cref="ChessPiece"/> objects keyed to a <see cref="Team"/> enum.</summary>
-    private readonly Dictionary<Team, Dictionary<string, ChessPiece>> _teamPieces;
+    private readonly Dictionary<Team, Dictionary<string, ChessPiece>> _chessPiecesByTeam;
 
     /// <summary>List of submitted moves within the current <see cref="GameEnvironment"/> instance.</summary>
     private readonly List<MovementInformation> _gameMoves = new();
@@ -70,7 +70,7 @@ public class GameEnvironment
         GameID = serverSideID;
         PlayerTeam = playerTeam;
 
-        _teamPieces = GenerateBoard();
+        _chessPiecesByTeam = GenerateBoard();
         AssignKings(out _whiteKing, out _blackKing);
     }
 
@@ -225,7 +225,7 @@ public class GameEnvironment
         if (!IsKingChecked(teamToCheck)) return false;
 
         // Determine if there are any moves that can be done to prevent the current check.
-        foreach (ChessPiece friendlyChessPiece in _teamPieces[teamToCheck].Values.Where(x => !x.Captured))
+        foreach (ChessPiece friendlyChessPiece in _chessPiecesByTeam[teamToCheck].Values.Where(x => !x.Captured))
         {
             foreach (var movement in AvailableMoves(friendlyChessPiece))
             {
@@ -242,13 +242,17 @@ public class GameEnvironment
     {
         if (_movesSinceLastCapture >= 50) return true;
         // Count Pieces on Board
-        int piecesRemaining = (from teams in _teamPieces.Values
+        int piecesRemaining = (from teams in _chessPiecesByTeam.Values
                                from piece in teams.Values
                                where piece.Captured == false
                                select piece).Count();
 
         if (piecesRemaining == 2) return true;
-        return false;
+
+        var moveAvailableForActiveTeam = (from piece in _chessPiecesByTeam[ActiveTeam].Values
+                                          where AvailableMoves(piece).Any()
+                                          select true).Any();
+        return !moveAvailableForActiveTeam;
     }
 
     /// <summary>
@@ -260,7 +264,7 @@ public class GameEnvironment
     {
         var teamMoves = new Dictionary<string, List<MovementInformation>>();
 
-        foreach (ChessPiece chessPiece in _teamPieces[queriedTeam].Values.Where(x => !x.Captured))
+        foreach (ChessPiece chessPiece in _chessPiecesByTeam[queriedTeam].Values.Where(x => !x.Captured))
         {
             teamMoves.Add(chessPiece.ID, AvailableMoves(chessPiece));
         }
@@ -364,22 +368,20 @@ public class GameEnvironment
 
                 // The local player isn't allowed to submit a move that will place themselves in check, So just determine if the opponent
                 // has placed the local player in check.
-                if (!localPlayerMove && IsKingCheckMated(PlayerTeam))
+
+                Team newActiveTeam = ActiveTeam = ReturnOppositeTeam(ActiveTeam);
+                DisableTeamVulnerabilityToEnPassant(newActiveTeam);
+                if (IsStalemate())
+                {
+                    ChangeGameState(GameState.GameDraw);
+                }
+                else if (!localPlayerMove && IsKingCheckMated(PlayerTeam))
                 {
                     ChangeGameState(GameState.LocalLoss);
                 }
                 else if (localPlayerMove && IsKingCheckMated(ReturnOppositeTeam(PlayerTeam)))
                 {
                     ChangeGameState(GameState.LocalWin);
-                }
-                else if (IsStalemate())
-                {
-                    ChangeGameState(GameState.GameDraw);
-                }
-                else
-                {
-                    Team newActiveTeam = ActiveTeam = ReturnOppositeTeam(ActiveTeam);
-                    DisableTeamVulnerabilityToEnPassant(newActiveTeam);
                 }
                 success = true;
             }
@@ -399,7 +401,7 @@ public class GameEnvironment
     /// <param name="activeTeam">The Team that will have its Pawns be no longer captureable via En Passant.</param>
     private void DisableTeamVulnerabilityToEnPassant(Team activeTeam)
     {
-        foreach (ChessPiece chessPiece in _teamPieces[activeTeam].Values.Where(x => x.CanBeCapturedViaEnPassant))
+        foreach (ChessPiece chessPiece in _chessPiecesByTeam[activeTeam].Values.Where(x => x.CanBeCapturedViaEnPassant))
         {
             chessPiece.DisableEnPassantCaptures();
         }
@@ -637,7 +639,7 @@ public class GameEnvironment
         {
             Team wantedTeam = (wantPrimary || move.CastlingWithSecondary) ? move.SubmittingTeam : ReturnOppositeTeam(move.SubmittingTeam);
             string pieceID = wantPrimary ? move.MainCopy.ID : move.SecondaryCopy!.ID;
-            pieceFromMove = _teamPieces[wantedTeam][pieceID];
+            pieceFromMove = _chessPiecesByTeam[wantedTeam][pieceID];
         }
         catch (KeyNotFoundException e)
         {
